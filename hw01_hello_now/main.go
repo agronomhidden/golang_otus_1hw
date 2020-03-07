7f3247a59bdfc3448e1f14f4760927544514c799
Dmitry <d.kirsanov@artepay.ru>
2020-03-07 23:52:25 +0300

    HW1 is completed #2

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -12,7 +11,7 @@ import (
 func main() {
 	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
-		log.Fatal(errors.New(fmt.Sprintf("unable to get remote time: %s", err)))
+		log.Fatal(fmt.Errorf("unable to get remote time: %s", err))
 	}
 	fmt.Println(fmt.Sprintf("current time: %s", time.Now().Format("2006-01-02 15:04:05 -0700 MST")))
 	fmt.Println(fmt.Sprintf("exact time: %s", ntpTime.Format("2006-01-02 15:04:05 -0700 MST")))
